4b7196110e9c72f1e60f55b154407ecf5c188554
SrFebry <febryanmdp@mhs.mdp.ac.id>
2023-12-18 19:26:44 +0700

    Initial commit

---
+++ lib/form_input.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MyForm extends StatefulWidget {
+  const MyForm({super.key});
+
+  @override
+  State<MyForm> createState() => _MyFormState();
+}
+
+class _MyFormState extends State<MyForm> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
